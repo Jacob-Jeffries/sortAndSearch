2fc261a829307f0ab21e9592a58326638c0c4b22
jacob@tiger <jeffrij85@gmail.com>
2024-10-27 16:32:50 -0400

    Created Array & List: bubblesort, selectionsort, binarySearch.

--- src/utilities/Collections.java
+++ src/utilities/Collections.java
@@ -1,8 +1,128 @@
 package utilities;
 
+import java.util.List;
+
 /**
  * Helper class with static methods.
  */
-public class Collections {
+public class Collections{
+
+  private static void swapArray(double[] data, int i, int j){
+    double tempValue = data[i];
+    data[i] = data[j];
+    data[j] = tempValue;
+  }
+
+  private static void swapList(List<Double> data, int i, int j){
+    double tempValue = data.get(i);
+    data.set(i, data.get(j));
+    data.set(j, tempValue);
+  }
+
+  private static int calcMiddle(int low, int high){
+    return (high + low) / 2;
+  }
+
+  public static void bubblesort(double[] data) {
+    for (int i = 0; i < data.length-1; ++i){
+      for (int j = 0; j < data.length-1; ++j){
+        if (data[j] > data[j+1]){
+          swapArray(data, j, (j+1));
+        }
+      }
+    }
+  }
+
+  public static void bubblesort(List<Double> data) {
+    for (int i = 0; i < data.size() - 1; ++i) {
+      for (int j = 0; j < data.size() - 1; ++j) {
+        if (data.get(j) > data.get(j + 1)) {
+          swapList(data, j, (j + 1));
+        }
+      }
+    }
+  }
+
+  public static void selectionsort(double[] data){
+    int smallestValue;
+    for (int pass = 0; pass < data.length-1; ++pass){
+      smallestValue = pass;
+      for (int index = pass; index < data.length; ++index){
+        if (data[index] < data[smallestValue]){
+          smallestValue = index;
+        }
+      }
+      swapArray(data, pass, smallestValue);
+    }
+  }
+
+  public static void selectionsort(List<Double> data) {
+    int smallestValue;
+    for (int pass = 0; pass < data.size() - 1; ++pass) {
+      smallestValue = pass;
+      for (int index = pass; index < data.size(); ++index) {
+        if (data.get(index) < data.get(smallestValue)) {
+          smallestValue = index;
+        }
+      }
+      swapList(data, pass, smallestValue);
+    }
+  }
+
+  public static int binarySearch(double[] data, double q){
+    int lI = 0;
+    int hI = data.length-1;
+    int mI = calcMiddle(lI, hI);
+
+    while (lI != mI){
+      if (mI == q){
+        return mI;
+      } else if (q < data[mI]){
+        hI = mI;
+        mI = calcMiddle(lI, hI);
+      } else {
+        lI =  mI;
+        mI = calcMiddle(lI, hI);
+      }
+    }
+
+    if (q == data[lI]){
+      return lI;
+    } else if (q == data[mI]){
+      return mI;
+    } else if (q == data[hI]) {
+      return hI;
+    } else {
+      return -1;
+    }
+  }
+
+  public static int binarySearch(List<Double> data, double q) {
+    int lI = 0;
+    int hI = data.size() - 1;
+    int mI = calcMiddle(lI, hI);
+
+    while (lI != mI) {
+      if (mI == q) {
+        return mI;
+      } else if (q < data.get(mI)) {
+        hI = mI;
+        mI = calcMiddle(lI, hI);
+      } else {
+        lI = mI;
+        mI = calcMiddle(lI, hI);
+      }
+    }
+
+    if (q == data.get(lI)) {
+      return lI;
+    } else if (q == data.get(mI)) {
+      return mI;
+    } else if (q == data.get(hI)) {
+      return hI;
+    } else {
+      return -1;
+    }
+  }
 
 }
--- src/utilities/CollectionsTest.java
+++ src/utilities/CollectionsTest.java
@@ -1,20 +1,18 @@
 package utilities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.List;
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CollectionsTest {
 
-  // This is a progenitor for further arrays.
   private double[] unsortedData = { 363.73, -742.84, 7.0, 32.42, -867.3, 567.67, 0.0, 10.56, 42.42 };
   private double[] sortedData = { -867.3, -742.84, 0.0, 7.0, 10.56, 32.42, 42.42, 363.73, 567.67 };
   private double[] arrayData;
-  // List<Double> is an interface, it can create a type but not an object.
   private List<Double> listData;
 
   @BeforeEach
@@ -34,7 +32,7 @@ public class CollectionsTest {
 
   @Test
   public void testBubblesortArray() {
-    Collections.bubblesort(arrayData); // Public, static, return void, parameter: array of Double.
+    Collections.bubblesort(arrayData);
     assertEquals(sortedData.length, arrayData.length);
     for (int i = 0; i < arrayData.length; ++i) {
       assertEquals(sortedData[i], arrayData[i]);
@@ -42,17 +40,17 @@ public class CollectionsTest {
   }
 
   @Test
-  public void testBublesortList() {
+  public void testBubblesortList() {
     Collections.bubblesort(listData);
     assertEquals(sortedData.length, listData.size());
-    for (int i = 0; i < listData.size(); ++i) {
+    for (int i = 0; i < sortedData.length; ++i) {
       assertEquals(sortedData[i], listData.get(i));
     }
   }
 
   @Test
   public void testSelectionsortArray() {
-    Collections.selectionsort(arrayData); // Public, static, return void, parameter: array of Double.
+    Collections.selectionsort(arrayData);
     assertEquals(sortedData.length, arrayData.length);
     for (int i = 0; i < arrayData.length; ++i) {
       assertEquals(sortedData[i], arrayData[i]);
@@ -63,24 +61,22 @@ public class CollectionsTest {
   public void testSelectionsortList() {
     Collections.selectionsort(listData);
     assertEquals(sortedData.length, listData.size());
-    for (int i = 0; i < listData.size(); ++i) {
+    for (int i = 0; i < sortedData.length; ++i) {
       assertEquals(sortedData[i], listData.get(i));
     }
   }
 
   @Test
   public void testBinarySearchArray() {
-
-    // Positive Tests.
-    int index = Collections.binarySearch(sortedData, -867.3); // returns index int.
+    // positive tests for values in the collection
+    int index = Collections.binarySearch(sortedData, -867.3);
     assertEquals(0, index);
     index = Collections.binarySearch(sortedData, 567.67);
     assertEquals(8, index);
     index = Collections.binarySearch(sortedData, 7.0);
     assertEquals(3, index);
-
-    // Negative Tests.
-    index = Collections.binarySearch(sortedData, -999.9); // returns index int.
+    // negative tests for values not in the collection
+    index = Collections.binarySearch(sortedData, -999.9);
     assertEquals(-1, index);
     index = Collections.binarySearch(sortedData, 568.0);
     assertEquals(-1, index);
@@ -90,22 +86,19 @@ public class CollectionsTest {
 
   @Test
   public void testBinarySearchList() {
-
     List<Double> sortedList = new ArrayList<Double>();
-    for(double eachDouble : sortedData){
+    for (double eachDouble : sortedData) {
       sortedList.add(eachDouble);
-    };
-
-    // Positive Tests.
-    int index = Collections.binarySearch(sortedList, -867.3); // returns index int.
+    }
+    // positive tests for values in the collection
+    int index = Collections.binarySearch(sortedList, -867.3);
     assertEquals(0, index);
     index = Collections.binarySearch(sortedList, 567.67);
     assertEquals(8, index);
     index = Collections.binarySearch(sortedList, 7.0);
     assertEquals(3, index);
-
-    // Negative Tests.
-    index = Collections.binarySearch(sortedList, -999.9); // returns index int.
+    // negative tests for values not in the collection
+    index = Collections.binarySearch(sortedList, -999.9);
     assertEquals(-1, index);
     index = Collections.binarySearch(sortedList, 568.0);
     assertEquals(-1, index);
